2aefbd609faca0fec543eb64085c618a9e5e381f
FranzCSUF <fsalvador79@gmail.com>
2018-06-13 21:55:24 -0700

    Adding corrections to string manipulation 3 warm up.

--- string-manipulation-3/main.js
+++ string-manipulation-3/main.js
@@ -1,18 +1,40 @@
 /* eslint-disable no-unused-vars */
 function padLeft(string, padding, maxLength) {
   var pad = ''
-  var padLength = (maxLength - string.length)
-  for (var i = 0; i < padLength; i++) {
-    pad += padding
+  var finalString = string + pad
+  if (padding.length > 1) {
+    for (var i = 0; i < maxLength - string.length; i++) {
+      if (pad.charAt(pad.length - 1) === padding[0]) {
+        pad += padding[1]
+      }
+      else pad += padding[0]
+    }
+  }
+  else {
+    var padLength2 = (maxLength - string.length)
+    for (var o = 0; o < padLength2; o++) {
+      pad += padding
+    }
   }
   return pad + string
 }
 
 function padRight(string, padding, maxLength) {
   var pad = ''
-  var padLength = (maxLength - string.length)
-  for (var i = 0; i < padLength; i++) {
-    pad += padding
+  var finalString = string + pad
+  if (padding.length > 1) {
+    for (var i = 0; i < maxLength - string.length; i++) {
+      if (pad.charAt(pad.length - 1) === padding[0]) {
+        pad += padding[1]
+      }
+      else pad += padding[0]
+    }
+  }
+  else {
+    var padLength2 = (maxLength - string.length)
+    for (var o = 0; o < padLength2; o++) {
+      pad += padding
+    }
   }
   return string + pad
 }
